74783ef49d11d4e9fd3ed6748f467277e455b042
Danyal Sheikh <Danyal Sheikh>
2017-04-29 04:48:27 +0500

    uploading project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Pollen Alarm</title>
+
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+		<link href="{{ asset('fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+		
+		<!--[if lt IE 9]>
+		<script src="{{ asset('js/ie-support/html5.js') }}"></script>
+		<script src="{{ asset('js/ie-support/respond.js') }}"></script>
+		<![endif]-->
+
+	</head>
+
+
+	<body>
+
+        <div class="site-content">
+            
+            @include('partials.header')
+            
+            @yield('content')
+            
+            @include('partials.footer')
+
+        </div>
+
+        <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
+	    <script src="{{ asset('js/plugins.js') }}"></script>
+	    <script src="{{ asset('js/app.js') }}"></script>
+
+	@yield('pageScripts')
+	@show   
+
+    </body>
+</html>
